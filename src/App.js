daa035c96305a2a541c2c16dd77958d2888c8201
Paul Gonzalez <robertgonzalez018@gmail.com>
2019-07-29 13:07:58 -0700

    added the snackbar feature from materialui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
-import Palette from './Palette';
-import seedColors from './seedColors';
-import { generatePalette } from './colorHelpers'; 
+import React, { Component } from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[4]))
+    console.log(generatePalette(seedColors[4]));
     return (
-      <div >
-       <Palette {...seedColors[4]} />
+      <div>
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -1,18 +1,46 @@
-import React, { Component } from 'react';
-import ColorBox from './ColorBox'
-import './Palette.css'
+//This is the code for my Palette.js component. 
+
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
+import Navbar from "./Navbar";
 
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500, format: "hex" };
+    this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+
+  changeLevel(level) {
+    this.setState({ level });
+  }
+
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
+
   render() {
-    const colorBoxes = this.props.colors.map(color => (
-      <ColorBox background={color.color} name={color.name} />
-    ))
+    const { colors } = this.props.palette;
+    const { level, format } = this.state;
+    const colorBoxes = colors[level].map(color => (
+      <ColorBox background={color[format]} name={color.name} />
+    ));
     return (
-      <div className="Palette" >
+      <div className="Palette">
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
+
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
-    ); 
+    );
   }
 }
 
 export default Palette;
+
+
