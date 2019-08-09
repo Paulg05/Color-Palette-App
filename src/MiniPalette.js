8cf2c3df0e0d23a89108a5027a8ede11676a6560
Paul Gonzalez <robertgonzalez018@gmail.com>
2019-08-09 13:25:32 -0700

    refactor with PureComponent

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,21 +1,28 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { withStyles } from "@material-ui/styles";
 //way to style our components without using a CSS file. ^
 import styles from "./styles/MiniPaletteStyles";
 import DeleteIcon from "@material-ui/icons/Delete";
 
-class MiniPalette extends Component {
+class MiniPalette extends PureComponent {
   constructor(props) {
     super(props);
     this.deletePalette = this.deletePalette.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
-
+  
   deletePalette(e) {
     e.stopPropagation();
     this.props.openDialog(this.props.id);
   }
+
+  handleClick() {
+    this.props.goToPalette(this.props.id)
+  }
+
   render() {
-    const { classes, paletteName, emoji, colors, handleClick } = this.props;
+    const { classes, paletteName, emoji, colors, handleClick, id } = this.props;
+    console.log("Rendering", paletteName)
     const miniColorBoxes = colors.map(color => (
       <div
         className={classes.miniColor}
@@ -24,7 +31,7 @@ class MiniPalette extends Component {
       />
     ));
     return ( 
-      <div className={classes.root} onClick={handleClick}>
+      <div className={classes.root} onClick={this.handleClick} >
         <DeleteIcon
           className={classes.deleteIcon}
           style={{ transition: "all 0.3s ease-in-out" }}
